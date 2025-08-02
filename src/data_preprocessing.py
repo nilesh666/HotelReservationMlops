c5e76f6c1d1f54377be5a0debc7f707ed81047aa
nilesh666 <nileshnandan.ts@gmail.com>
2025-08-02 08:20:16 +0530

    Modified data preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -10,3 +10,27 @@ from sklearn.preprocessing import LabelEncoder
 from imblearn.over_sampling import SMOTE
 
 logger = get_logger(__name__)
+
+class DataProcessor:
+    def __init__(self, train_path, test_path, processed_dir, config_path):
+        self.train_path = train_path
+        self.test_path = test_path
+        self.processed_dir = processed_dir
+        self.config = read_yaml(config_path)
+
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+
+    def preprocess_data(self, df):
+        try:
+            logger.info("Started data preprocessing")
+
+            df.drop(columns = ['Unnamed: 0','Booking_ID'] , axis = 1, inplace=True)
+            logger.info("Dropped booking_id and unamed: 0 columns")
+            
+            df.drop_duplicates(inplace=True)
+            logger.inof("Removed duplicates")
+
+        except Exception as e:
+            logger.error("Error preprocessing the data")
+            raise CustomException("Error preprocessing the data", e)
